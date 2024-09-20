6bc85a6833b21b5c878fa631cc2bec15c741bd89
jax-01 <jrcastillo6@up.edu.ph>
2024-09-20 13:58:39 +0800

    Revert "Get the container of the grid"
    This reverts commit f9c4e673254de4eda2b43737c2fdfc1aeff2ee84.

--- script.js
+++
@@ -1,7 +0,0 @@
-const container = document.querySelector("#container");
-
-for (let i = 0; i < 16 * 16; i++) {
-  const grid = document.createElement("div");
-  grid.classList.add("grid");
-  container.appendChild(grid);
-}
